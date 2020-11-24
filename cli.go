fb021d326ad044f8c746615a8f9bb5c9915a7336
18yang <SubjuGateW@163.com>
2020-11-24 13:33:05 +0800

    错误已改正

--- cli.go
+++ cli.go
@@ -5,10 +5,12 @@ import (
 	"os"
 	"strconv"
 )
+
 //这是一个用来接受命令行参数并且控制区块链操作的文件
 type CLI struct {
 	bc *BlockChain
 }
+
 const Usage = `
 	addBlock --data DATA       "添加区块"
 	printChain                 "正向打印区块链"
@@ -16,8 +18,9 @@ const Usage = `
 	getBalance --address ADDRESS "获取指定地址的余额"
 	send FROM TO AMOUNT MINER DATA "由FROM转AMOUNT给TO,由MINER挖矿,同时写入DATA"
 `
+
 //接收参数的动作，放在一个函数中
-func (cli *CLI)Run()  {
+func (cli *CLI) Run() {
 	//得到所有的命令
 	args := os.Args
 	// ./block printChain
@@ -29,20 +32,20 @@ func (cli *CLI)Run()  {
 	//2. 分析命令
 	cmd := args[1]
 	switch cmd {
-		//3. 执行相应的动作
+	//3. 执行相应的动作
 	case "addBlock":
 		//添加区块
 		fmt.Println("添加区块")
-		if len(args) ==4 && args[2] == "--data"  {
+		if len(args) == 4 && args[2] == "--data" {
 			//获取命令的数据
 			data := args[3]
 			//添加区块
 			cli.AddBlock(data)
-		}else{
+		} else {
 			fmt.Println("参数使用不当，请检查！")
 			fmt.Print(Usage)
 		}
-	case "printChain" :
+	case "printChain":
 		fmt.Println("正向打印区块链\n")
 		cli.PrintBlockChain()
 	case "printChainR":
@@ -51,23 +54,23 @@ func (cli *CLI)Run()  {
 		cli.PrintBlockChainReverse()
 	case "getBalance":
 		fmt.Printf("获取余额\n")
-		if len(args) ==4 && args[2] == "--address"  {
+		if len(args) == 4 && args[2] == "--address" {
 			address := args[3]
 			cli.GetBalance(address)
 		}
 	case "send":
-		if len(args) != 7{
+		if len(args) != 7 {
 			fmt.Printf("参数个数错误，请检查！")
 			fmt.Printf(Usage)
-		}else {
-			fmt.Printf("开始转账...\n")
-			from := args[2]
-			to := args[3]
-			amount,_ := strconv.ParseFloat( args[4] , 64 )
-			miner := args[5]
-			data := args[6]
-			cli.Send(from,to,amount,miner,data)
+			return
 		}
+		fmt.Printf("开始转账...\n")
+		from := args[2]
+		to := args[3]
+		amount, _ := strconv.ParseFloat(args[4], 64)
+		miner := args[5]
+		data := args[6]
+		cli.Send(from, to, amount, miner, data)
 
 	default:
 		fmt.Printf(Usage)
--- transaction.go
+++ transaction.go
@@ -7,13 +7,16 @@ import (
 	"fmt"
 	"log"
 )
-const reward = 25
+
+const reward = 50
+
 //定义交易结构
 type Transaction struct {
-	TXID []byte  //交易id
-	TXInputs []TXInput //交易输入数组
+	TXID      []byte     //交易id
+	TXInputs  []TXInput  //交易输入数组
 	TXoutputs []TXOutput //交易输出数组
 }
+
 //定义交易输入
 type TXInput struct {
 	//引用的交易ID
@@ -23,6 +26,7 @@ type TXInput struct {
 	//解锁脚本，我们用地址来模拟
 	Sig string
 }
+
 //定义交易输出
 type TXOutput struct {
 	//转账金额
@@ -30,8 +34,9 @@ type TXOutput struct {
 	//锁定脚本，用地址模拟
 	PukKeyHash string
 }
+
 //设置交易ID
-func (tx *Transaction) SetHash(){
+func (tx *Transaction) SetHash() {
 	var buffer bytes.Buffer
 	encoder := gob.NewEncoder(&buffer)
 
@@ -43,6 +48,7 @@ func (tx *Transaction) SetHash(){
 	hash := sha256.Sum256(data)
 	tx.TXID = hash[:]
 }
+
 //实现一个函数，判断当前的交易是否为挖矿交易
 func (tx *Transaction) IsCoinbase() bool {
 	//1. 交易input只有一个
@@ -55,7 +61,7 @@ func (tx *Transaction) IsCoinbase() bool {
 }
 
 //提供创建交易方法(挖矿交易)
-func NewCoinBaseTX(address string,data string) *Transaction{
+func NewCoinBaseTX(address string, data string) *Transaction {
 	//挖矿交易的特点：
 	//1. 只有一个input
 	//2. 无需引用交易id
@@ -79,41 +85,42 @@ func NewCoinBaseTX(address string,data string) *Transaction{
 	tx.SetHash()
 	return &tx
 }
+
 //创建普通交易
-func NewTransaction(from, to string,amount float64,bc *BlockChain) *Transaction{
-	//TODO
+func NewTransaction(from, to string, amount float64, bc *BlockChain) *Transaction {
 	//1. 找到最合理的utxo集合
-	utxos, resValue := bc.FindNeedUTXOs(from,amount)
+	utxos, resValue := bc.FindNeedUTXOs(from, amount)
 
 	if resValue < amount {
 		fmt.Printf("余额不足，交易失败")
+		return nil
 	}
 
 	var inputs []TXInput
 	var outputs []TXOutput
 	//2. 将utxo逐一装成inputs   创建交易输入
 	for id, indexArray := range utxos {
-		for _,i := range indexArray {
+		for _, i := range indexArray {
 			input := TXInput{
 				TXid:  []byte(id),
 				Index: int64(i),
 				Sig:   from,
 			}
-			inputs = append(inputs,input)
+			inputs = append(inputs, input)
 		}
 	}
 	//3. 创建outputs 创建交易输出
-	output:= TXOutput{
+	output := TXOutput{
 		Value:      amount,
 		PukKeyHash: to,
 	}
-	outputs = append(outputs,output)
+	outputs = append(outputs, output)
 	//4. 如果有零钱，找零
 	if resValue > amount {
-		outputs = append(outputs,TXOutput{resValue-amount,from})
+		outputs = append(outputs, TXOutput{resValue - amount, from})
 	}
 	//将交易输入与输出添加到一个交易中
-	tx := Transaction{[]byte{},inputs,outputs}
+	tx := Transaction{[]byte{}, inputs, outputs}
 	tx.SetHash()
 	return &tx
 }
